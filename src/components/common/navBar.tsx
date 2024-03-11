9256bc262ea6156c1e66fda3c09b7975deaa0b61
Ian Talaber <ian.talaber@accenture.com>
2024-03-11 07:41:05 +0100

    links to github and linkedin added

--- src/components/common/navBar.tsx
+++ src/components/common/navBar.tsx
@@ -36,6 +36,38 @@ function NavBar() {
               Skills
             </Link>
           </li>
+          <li>
+            <a
+              data-testid="github-link"
+              href="https://github.com/italaber2"
+              className="github-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/github-mark-white.png"
+                alt="github"
+                width="25"
+                height="25"
+                style={{ cursor: "pointer" }}
+              />
+            </a>
+            <a
+              data-testid="linkedin-link"
+              href="https://linkedin.com/in/italaber"
+              className="linkedin-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/linkedin-logo.png"
+                alt="linkedin"
+                width="25"
+                height="25"
+                style={{ cursor: "pointer" }}
+              />
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
