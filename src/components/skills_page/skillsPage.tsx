3fa25fe45486a42db05e5d9211becce8cc265cff
Ian Talaber <ian.talaber@accenture.com>
2024-04-17 17:14:11 +0200

    initial button logic for skills page

--- src/components/skills_page/skillsPage.tsx
+++ src/components/skills_page/skillsPage.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderElement from "../common/headerElement";
+import MyComponent from "./radarChart";
+import jsonData from "../../data/projects.json";
+
+interface ProjectData {
+  id: number;
+  title: string;
+}
+
+function ButtonWrapper(setSelectedProjectId: (id: number) => void) {
+  return (
+    <div className="button-wrapper">
+      {jsonData.map((item: ProjectData) => (
+        <button
+          key={item.id}
+          onClick={() => {
+            setSelectedProjectId(item.id);
+          }}
+        >
+          {item.title}
+        </button>
+      ))}
+    </div>
+  );
+}
 
 function SkillsPage() {
+  const [selectedProjectId, setSelectedProjectId] = useState(0);
   return (
     <div className="skills">
       <React.Fragment>
         <HeaderElement headerId={3} />
       </React.Fragment>
       <React.Fragment>
-        <iframe
-          src="https://giphy.com/embed/6RrGJniqA0zD07caO1"
-          title="Still Growing"
-        ></iframe>
+        <ButtonWrapper
+          setSelectedProjectId={(id: number) => {
+            return setSelectedProjectId(id);
+          }}
+        ></ButtonWrapper>
+        {/* Each button should take the json.title as it's text and json.id as the value */}
+        {/* useState variable to set selected project id and hand it over to MyComponent */}
+        <MyComponent></MyComponent>
       </React.Fragment>
     </div>
   );
