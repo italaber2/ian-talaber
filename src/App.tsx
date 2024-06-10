e8a8e47c3ea3c163958c340158d59e65d398e843
Ian Talaber <ian.talaber@accenture.com>
2024-06-10 13:59:42 +0200

    redirect page created

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <div className="app">
       <Switch>
         <Route path="/" component={PromoPage} />
+        <Route path="/redirect" component={PromoPage} />
         <Route path="/projects" component={ProjectsPage} />
         <Route path="/skills" component={SkillsPage} />
       </Switch>
