b0197746e15d4b412fe4be63a36696c7c7082078
Ian Talaber <ian.talaber@accenture.com>
2024-07-03 10:26:51 +0200

    initial playwright test fixed

--- tests/not-xkcd.spec.ts
+++ tests/not-xkcd.spec.ts
@@ -1,19 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("correct url", async ({ page }) => {
-  await page.goto("https://not-xkcd.vercel.app/");
+  await page.goto("https://iantalaber.com/");
 
-  await expect(page).toHaveURL("https://not-xkcd.vercel.app/");
+  await expect(page).toHaveURL("https://iantalaber.com/");
 });
-
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
-
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
